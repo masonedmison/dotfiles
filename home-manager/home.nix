0acad3e56aed15c59a099b43d9b51e75b9dc87a4
Mason Edmison <mason.edmison@gmail.com>
2024-07-30 15:33:43 -0500

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,8 +1,6 @@
 { pkgs, kmonad, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home = {
 
     username = "edmisml";
@@ -11,25 +9,15 @@
       LANG = "en_US.UTF-8";
       EDITOR = "nvim";
     };
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
     stateVersion = "22.11";
 
     packages = with pkgs; [
       bottom
       cargo
       chicken
-      # clang
       curl
       discord
       duf
-      exa
       fd
       fish
       fx
@@ -38,7 +26,6 @@
       httpie
       jless
       kmonad
-      # llvm
       nil
       obsidian
       p7zip
