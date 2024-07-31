5c46ba3129683de817f8e3e971ffbb37a8005fc2
Mason Edmison <mason.edmison@gmail.com>
2024-07-31 13:51:40 -0500

    further refine firefox config

--- home-manager/programs/firefox.nix
+++ home-manager/programs/firefox.nix
@@ -1,6 +1,14 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.firefox = {
     enable = true;
+    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      extraPolicies = {
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "always";
+      };
+    };
     profiles."edmisml" = {
         id = 0;
         name = "default";
@@ -8,6 +16,22 @@
           "browser.startup.homepage" = "https://google.com";     
           "browser.urlbar.placeholderName" ="Google";
           "browser.shell.checkDefaultBrowser" = false;
+        };
+        search = {
+          force = true;
+          engines = {
+            "Nix Packages" = {
+              urls = [{
+                template = "https://search.nixos.org/packages";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+          };
         };
     };
   };
