4bc1be37f2d66defe49dd3e9aed50a11d49664cc
Mason Edmison <mason.edmison@gmail.com>
2024-08-08 14:44:05 -0500

    manually provide neovim config

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -1,16 +1,5 @@
 { pkgs, ... }: {
   programs.neovim = {
     enable = true;
-    extraConfig = "syntax on";
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
-      rec {
-        plugin = onedark-vim;
-        config = ''
-          packadd! ${plugin.pname}
-          colorscheme onedark
-        '';
-      }
-    ];
   };
 }
