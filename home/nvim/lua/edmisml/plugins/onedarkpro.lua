3705f805a41736fe14ed156fa8ff81fd96186f60
Mason Edmison <mason.edmison@gmail.com>
2024-08-10 20:50:44 -0500

    switch onedark pro impl

--- home/nvim/lua/edmisml/plugins/onedarkpro.lua
+++ home/nvim/lua/edmisml/plugins/onedarkpro.lua
@@ -1,7 +1,12 @@
 return {
-  'olimorris/onedarkpro.nvim',
+  'navarasu/onedark.nvim',
   priority = 1000, -- Ensure it loads first
   init = function()
     vim.cmd.colorscheme 'onedark'
+  end,
+  config = function()
+    require('onedark').setup {
+      style = 'cool',
+    }
   end,
 }
