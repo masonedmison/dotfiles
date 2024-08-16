0b18ffc031e86976c826484abe42508dc7fe4811
Mason Edmison <mason.edmison@gmail.com>
2024-08-15 20:48:55 -0500

    mapping for all metals commands

--- home/nvim/lua/edmisml/plugins/telescope.lua
+++ home/nvim/lua/edmisml/plugins/telescope.lua
@@ -85,6 +85,10 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<leader>wm', function()
+        require('telescope').extensions.metals.commands()
+      end, { desc = '[W]orkspace [M]etals Commands.' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
