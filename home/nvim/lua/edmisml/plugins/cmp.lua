e7852182c317758adaef3bde12aa496281981181
Mason Edmison <mason.edmison@gmail.com>
2024-08-23 11:52:32 -0500

    add treesitter as source for nvim-cmp

--- home/nvim/lua/edmisml/plugins/cmp.lua
+++ home/nvim/lua/edmisml/plugins/cmp.lua
@@ -34,6 +34,7 @@ return {
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'ray-x/cmp-treesitter',
     },
     config = function()
       -- See `:help cmp`
@@ -67,12 +68,6 @@ return {
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -108,6 +103,7 @@ return {
             group_index = 0,
           },
           { name = 'nvim_lsp' },
+          { name = 'treesitter' },
           { name = 'luasnip' },
           { name = 'path' },
         },
