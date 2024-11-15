1a8daea0be7fd5d3b75aa7831cbe8cdc94b0240f
Mason Edmison <mason.edmison@gmail.com>
2024-11-15 12:05:45 -0600

    color scheme tweaks

--- home/nvim/lua/edmisml/plugins/nightfox.lua
+++ home/nvim/lua/edmisml/plugins/nightfox.lua
@@ -1,6 +1,16 @@
 return {
   'EdenEast/nightfox.nvim',
+  priority = 1000,
   init = function()
     vim.cmd.colorscheme 'nightfox'
+  end,
+  config = function()
+    require('nightfox').setup {
+      options = {
+        styles = {
+          keywords = 'italic,bold',
+        },
+      },
+    }
   end,
 }
