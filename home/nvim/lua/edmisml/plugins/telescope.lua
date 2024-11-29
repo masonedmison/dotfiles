3ffc52a85f2852d855e2b5971ee89b7a16c89668
Mason Edmison <mason.edmison@gmail.com>
2024-11-29 10:18:41 -0600

    add shortcut to search obsidian notes

--- home/nvim/lua/edmisml/plugins/telescope.lua
+++ home/nvim/lua/edmisml/plugins/telescope.lua
@@ -147,6 +147,11 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+      --
+      -- Shortcut for searching your obsidian notes files
+      vim.keymap.set('n', '<leader>so', function()
+        builtin.find_files { cwd = vim.fn.expand '~/vaults/' }
+      end, { desc = '[S]earch [O]bsidian vault' })
     end,
   },
 }
