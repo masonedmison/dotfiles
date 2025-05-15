db0af675521a15208652d892a69427cc68f1613c
Mason Edmison <mason.edmison@gmail.com>
2025-05-15 09:43:00 -0500

    add lazygit to "ignore-able" list in close all buffers

--- home/nvim/lua/keymaps.lua
+++ home/nvim/lua/keymaps.lua
@@ -4,29 +4,33 @@
 -- unload buffer
 vim.keymap.set('n', '<leader>bu', ':bd <CR>', { desc = '[B]uffer [U]nload' })
 
--- Close all buffers but the current buffer not including toggleterm buffers.
+-- Close all buffers but the current buffer not including "ignore-able" buffers.
 -- If the current buffer is a toggleterm terminal, this is a noop.
 vim.keymap.set('n', '<leader>bc', function()
   local bufs = vim.api.nvim_list_bufs()
   local current_buf = vim.api.nvim_get_current_buf()
+  local ignores = { 'lazygit' }
   -- get buffers that are not toggleterm buffers
-  local function non_toggterm(bufs)
-    local non_toggterms = {}
+  local function non_ignores(bufs)
+    local non_ignores_arr = {}
     local toggterm_curr = false
     for _, bi in ipairs(bufs) do
       local bufname = vim.api.nvim_buf_get_name(bi)
       local maybe_togg_term = string.find(bufname, 'toggleterm')
-      if not maybe_togg_term then
-        table.insert(non_toggterms, bi)
+      local is_ignore = #vim.tbl_filter(function(ig)
+        return string.find(bufname, ig) ~= nil
+      end, ignores) > 0
+      if not maybe_togg_term and not is_ignore then
+        table.insert(non_ignores_arr, bi)
       elseif maybe_togg_term and bi == current_buf then
         toggterm_curr = true
         break
       end
     end
 
-    return toggterm_curr, non_toggterms
+    return toggterm_curr, non_ignores_arr
   end
-  local curr_is_term, filtered_buffs = non_toggterm(bufs)
+  local curr_is_term, filtered_buffs = non_ignores(bufs)
   if curr_is_term then
     print 'Current buffer is a terminal!'
   else
