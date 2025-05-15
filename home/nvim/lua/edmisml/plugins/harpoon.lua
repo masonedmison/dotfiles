31f46dedfa68a5c40a5cf8db1a353b60149accd1
Mason Edmison <mason.edmison@gmail.com>
2025-05-15 09:42:44 -0500

    change harpoon shortcuts so it doesn't cause latency on "h" key

--- home/nvim/lua/edmisml/plugins/harpoon.lua
+++ home/nvim/lua/edmisml/plugins/harpoon.lua
@@ -6,44 +6,29 @@ return {
     local harpoon = require 'harpoon'
 
     harpoon.setup {
-      global_settings = {
+      settings = {
         mark_branch = true,
         tabline = false,
         tabline_prefix = '  ',
         tabline_suffix = '  ',
+        save_on_toggle = true,
       },
     }
-    --
-    -- basic telescope configuration
-    require('telescope').load_extension 'harpoon'
 
-    --vim.keymap.set('n', 'hm', ':Telescope harpoon marks<CR>', { desc = 'Open harpoon window' })
-    vim.keymap.set('n', 'hm', function()
+    vim.keymap.set('n', ',m', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
-    vim.keymap.set('n', 'hx', function()
+    vim.keymap.set('n', ',x', function()
       harpoon:list():add()
     end)
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', 'hp', function()
+    vim.keymap.set('n', ',p', function()
       harpoon:list():prev()
     end)
-    vim.keymap.set('n', 'hn', function()
+    vim.keymap.set('n', ',n', function()
       harpoon:list():next()
-    end)
-    vim.keymap.set('n', 'ha', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', 'hs', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', 'hd', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', 'hf', function()
-      harpoon:list():select(4)
     end)
   end,
 }
